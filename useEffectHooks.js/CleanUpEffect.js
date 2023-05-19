1d7512f871409e1c553317dffa46492228b05c91
Sarah Carlock <sarah.carlock86@gmail.com>
2023-05-19 13:17:31 -0500

    intial

---
+++ useEffectHooks.js/CleanUpEffect.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Counter() {
+
+  const [clickCount, setClickCount] = useState(0);
+  const increment = () => setClickCount((clickCount) => clickCount + 1);
+  useEffect(() =>{
+    document.addEventListener('mousedown', increment);
+    return () => {
+    document.removeEventListener('mousedown', increment);
+    };
+  })
+
+  return (
+      <h1>Document Clicks: {clickCount}</h1>
+  );
+}
